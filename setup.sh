f244ee1c0f780e60b944ea1bc6caa8e36670ef63
hydai <z54981220@gmail.com>
2018-03-23 14:32:23 +0800

    all: initialize and update git submodule after cloning dotfiles

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 
 git clone https://github.com/hydai/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
+git submodule update --init --recursive
 git remote set-url --push origin git@github.com:hydai/dotfiles.git
 ./install
