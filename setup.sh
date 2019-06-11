789f8e11358d2e713b911bedae1d3f9befc0ade8
hydai <hydai@hyd.ai>
2019-06-11 04:39:25 +0000

    scripts: add dependencies packages

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+case `uname` in
+  Linux)
+    sudo -H apt update
+    sudo -H apt install vim git zsh build-essential cmake tmux
+  ;;
+esac
+
 git clone https://github.com/hydai/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 git submodule update --init --recursive
